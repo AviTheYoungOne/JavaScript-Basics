92edd194c0944fea59a9f43fb8fc776aff12d51e
AviTheYoungOne <32620712+AviTheYoungOne@users.noreply.github.com>
2017-10-08 12:39:34 -0700

    Add type.js

---
+++ js_types.js
@@ -0,0 +1,19 @@
+'use strict' //Enforces strict rules and throws errors if there is a bug
+
+let favoriteNumber; // Declares a variable
+
+favoriteNumber = 999879; //Assigning a variable a value of 7
+
+let favoriteFood = 'Pizza' // Declares a variable and assigns a value of 'momo'
+
+
+console.log('My favorite number is:' + favoriteNumber)
+
+// JavaScript types
+// Numbers
+//Strings
+
+//Booleans
+//Functions
+
+console.log('My favorite food is:' + favoriteFood)
