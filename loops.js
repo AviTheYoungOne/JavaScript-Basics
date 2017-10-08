9d31e33fb3998c435f7c6a1a48829d86d22cc0fc
AviTheYoungOne <32620712+AviTheYoungOne@users.noreply.github.com>
2017-10-08 12:32:38 -0700

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,28 @@
+//for(var i = 0;i <=150; i = i + 5){
+ // console.log(i, 'random stuff')
+//}
+// console.log('2 * 1 =', 2 * 1)
+// console.log('2 * 2 =', 2 * 2)
+// console.log('2 * 3 =', 2 * 3)
+// console.log('2 * 4 =', 2 * 4)
+// console.log('2 * 5 =', 2 * 5)
+// console.log('2 * 6 =', 2 * 6)
+// console.log('2 * 7 =', 2 * 7)
+// console.log('2 * 8 =', 2 * 8)
+// console.log('2 * 9 =', 2 * 9)
+// console.log('2 * 10 =', 2 * 10)
+
+
+console.log('this is the multiplication table of two')
+for(var i = 1;i <= 10;i = i + 1){
+  console.log('2 *', i,' = ',2 * i )
+}
+
+
+
+
+console.log('this is the multiplication table of five')
+
+for(var d = 1; d <= 10; d = d + 1) {
+   console.log('5 *',d,' = ', 5 * d)
+}
