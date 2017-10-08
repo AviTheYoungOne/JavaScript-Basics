b28cf43e2f20faeb18a5acd451e889180fde521a
AviTheYoungOne <32620712+AviTheYoungOne@users.noreply.github.com>
2017-10-08 12:41:12 -0700

    Add menu for 85 degrees.js

---
+++ 85_degrees_menu.js
@@ -0,0 +1,23 @@
+console.log('Welcome to 85 Degrees')
+
+
+
+console.log('1.Iced Tea.........$3.99');
+console.log('2.Garlic Bread.....$2.99');
+console.log('3.Cookies..........$1.00');
+
+
+var customerchoice = 3;
+
+if(customerchoice == 1) {
+  console.log('You chose Iced Tea. Your total is $3.99')
+}
+if(customerchoice == 2) {
+  console.log('You chose Garlic Bread. Your total is $2.99')
+}
+if(customerchoice == 3) {
+  console.log('You chose Cookies. Your total is $1.00')
+}
+
+
+
